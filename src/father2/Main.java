a99ace482f3311a3ddb69c4bbd98d65bd0a39d84
peter <ylkangpeter@gmail.com>
2014-10-14 17:07:32 +0800

    hiho week15

--- src/father2/Main.java
+++ src/father2/Main.java
@@ -4,13 +4,16 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Scanner;
+import java.util.Stack;
 
 public class Main {
 
-	static ThreadLocal<T> t = new ThreadLocal<T>();
-	
 	private static class Node {
+
+		private static final byte in = 1;
+		private static final byte out = 2;
 
 		private byte val = 0;
 		private Node father = null;
@@ -24,38 +27,6 @@ public class Main {
 		public Node(String name, Node father) {
 			this.name = name;
 			this.father = father;
-		}
-
-		public byte getVal() {
-			return val;
-		}
-
-		public void setVal(byte val) {
-			this.val = val;
-		}
-
-		public Node getFather() {
-			return father;
-		}
-
-		public void setFather(Node father) {
-			this.father = father;
-		}
-
-		public String getName() {
-			return name;
-		}
-
-		public void setName(String name) {
-			this.name = name;
-		}
-
-		public List<Node> getChildren() {
-			return children;
-		}
-
-		public void setChildren(List<Node> children) {
-			this.children = children;
 		}
 
 		@Override
@@ -74,9 +45,16 @@ public class Main {
 		}
 	}
 
-	static Map<String, Node> nodeMap = new HashMap<String, Node>();
+	private static Map<String, Node> nodeMap = new HashMap<String, Node>();
 
-	static Node root;
+	/**
+	 * question a & b will be saved twice (<a,b> <b,a>) for search convenience
+	 */
+	private static Map<String, Map<String, List<Integer>>> questionMap = new HashMap<String, Map<String, List<Integer>>>();
+
+	private static String[] answer;
+
+	private static Stack<Node> tracingNodeStack = new Stack<Node>();
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
@@ -84,21 +62,120 @@ public class Main {
 			int total = in.nextInt();
 			if (total <= 0)
 				return;
-			root = new Node(in.next());
-			Node n = new Node(in.next(), root);
+			String fatherName = in.next();
+			Node root = new Node(fatherName);
+			String sonName = in.next();
+			Node n = new Node(sonName, root);
+			root.children.add(n);
+			nodeMap.put(fatherName, root);
+			nodeMap.put(sonName, n);
 
-			for (int i = 0; i < total - 2; i++) {
-				String fatherName = in.next();
+			for (int i = 0; i < total - 1; i++) {
+				fatherName = in.next();
 				Node father = buildNode(fatherName);
-				String sonName = in.next();
+				sonName = in.next();
 				Node son = new Node(sonName, father);
+				father.children.add(son);
 				nodeMap.put(fatherName, father);
 				nodeMap.put(sonName, son);
+			}
+			printTree(root, 0);
+
+			int totalQuestion = in.nextInt();
+			answer = new String[totalQuestion];
+			for (int i = 0; i < totalQuestion; i++) {
+				String name1 = in.next();
+				String name2 = in.next();
+				if (name1.equals(name2)) {
+					answer[i] = name1;
+				} else {
+					Map<String, List<Integer>> m = questionMap.get(name1);
+					if (m == null) {
+						m = new HashMap<String, List<Integer>>();
+						List<Integer> list = new ArrayList<Integer>();
+						list.add(i);
+						m.put(name2, list);
+					} else {
+						List<Integer> list = m.get(name2);
+						if (list == null) {
+							list = new ArrayList<Integer>();
+							m.put(name2, list);
+						}
+						list.add(i);
+					}
+					questionMap.put(name1, m);
+
+					m = questionMap.get(name2);
+					if (m == null) {
+						m = new HashMap<String, List<Integer>>();
+						List<Integer> list = new ArrayList<Integer>();
+						list.add(i);
+						m.put(name1, list);
+					} else {
+						List<Integer> list = m.get(name1);
+						if (list == null) {
+							list = new ArrayList<Integer>();
+							m.put(name1, list);
+						}
+						list.add(i);
+					}
+					questionMap.put(name2, m);
+				}
+			}
+
+			// dfs tree
+			dfs(root);
+			for (String name : answer) {
+				System.out.println(name);
 			}
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 
+	}
+
+	private static void dfs(Node node) {
+		tracingNodeStack.add(node);
+		node.val++;
+		Map<String, List<Integer>> values = questionMap.get(node.name);
+		if (values != null) {
+			for (Entry<String, List<Integer>> entry : values.entrySet()) {
+				String questionPart2 = entry.getKey();
+				if (nodeMap.get(questionPart2).val == Node.in) {
+					// key1 in, key2 in, then key2 is father
+					for (int inx : entry.getValue()) {
+						answer[inx] = questionPart2;
+					}
+				} else if (nodeMap.get(questionPart2).val == Node.out) {
+					// key1 in, key2 out
+					Node n = nodeMap.get(questionPart2);
+					Node father = getLatestInNode(n);
+					for (int inx : entry.getValue()) {
+						answer[inx] = father.name;
+					}
+					n.father = father;
+				}
+
+			}
+		}
+		if (!node.children.isEmpty()) {
+			for (Node child : node.children) {
+				dfs(child);
+			}
+		}
+		// leave this node
+		node.val++;
+		tracingNodeStack.pop();
+	}
+
+	private static Node getLatestInNode(Node n) {
+		if (n.val == Node.in) {
+			return n;
+		}
+		if (n.father != null) {
+			return getLatestInNode(n.father);
+		}
+		return n;
 	}
 
 	private static Node buildNode(String name) {
@@ -109,4 +186,15 @@ public class Main {
 		return node;
 	}
 
+	public static void printTree(Node node, int level) {
+		// System.out.print(level + "|");
+		for (int i = 0; i < level; i++) {
+			System.out.print("\t");
+		}
+		System.out.println(node.name);
+		for (Node child : node.children) {
+			printTree(child, level + 1);
+		}
+
+	}
 }
